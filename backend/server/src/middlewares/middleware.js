fa9f8c59f0272467c98adb52925495b13389d85a
Yash Chokhani <yashchokhani95@gmail.com>
2025-01-15 11:24:31 +0530

    feat: middleware added

---
+++ backend/server/src/middlewares/middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+class AppError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+    this.name = "AppError";
+  }
+}
+
+const handleAsyncError = (fn) => (req, res, next) => {
+  fn(req, res, next).catch((err) => next(err));
+};
+
+const isValidUser = handleAsyncError(async (req, res, next) => {
+  const userId = req.body.userId || req.params.userId;
+  const access_token = req.headers.access_token;
+  if (!access_token) throw new AppError("Unauthorized", 401);
+  const decoded = jwt.decode(access_token, { complete: true });
+  if (!decoded) throw new AppError("unauthorized", 401);
+  if (userId !== decoded.payload.sub) throw new AppError("unauthorized", 401);
+  next();
+});
+
+module.exports = { AppError, handleAsyncError, isValidUser };
