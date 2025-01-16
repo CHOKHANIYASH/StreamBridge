00729ded4816460b1a806ebc25e37f201e13d05a
Yash Chokhani <yashchokhani95@gmail.com>
2025-01-17 01:57:57 +0530

    fix: Key of object using short-uuid

--- backend/server/src/routes/videoRoutes.js
+++ backend/server/src/routes/videoRoutes.js
@@ -6,6 +6,8 @@ const {
 } = require("../middlewares/middleware");
 const videoControllers = require("../controllers/videoControllers");
 const { v4: uuidv4 } = require("uuid");
+const short = require("short-uuid");
+
 router.get(
   "/",
   handleAsyncError(async (req, res) => {
@@ -26,15 +28,14 @@ router.post(
   // isValidUser,
   handleAsyncError(async (req, res) => {
     const { contentType, userId, name } = req.body;
+    console.log("userId", userId);
     if (contentType !== "video/mp4")
       throw new AppError("Only Mp4 files are allowed", 400);
-    const key = `${userId}_${new Date().toISOString()}.${
-      contentType.split("/")[1]
-    }`;
+    const key = `${userId}_${short.generate()}.${contentType.split("/")[1]}`;
     const url = await videoControllers.preSignedUploadUrl({
       key,
     });
-    const videoId = uuidv4();
+    const videoId = key;
     await videoControllers.addVideoBuffer({ videoId, name, userId });
     res.status(200).send({
       success: true,
