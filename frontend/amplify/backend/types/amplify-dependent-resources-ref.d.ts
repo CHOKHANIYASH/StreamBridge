7d0d79a4cb8c81524a72864f2894aa7c5c889ebe
Yash Chokhani <yashchokhani95@gmail.com>
2025-02-18 02:22:48 +0530

    Fix Amplify

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "frontend72c817d1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- frontend/app/Amplify.jsx
+++ frontend/app/Amplify.jsx
@@ -2,7 +2,8 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 
-import outputs from "@/amplify_outputs.json";
+// import outputs from "@/amplify_outputs.json";
+import outputs from "@/src/aws-exports";
 // console.log("outputs = ", outputs);
 Amplify.configure({ ...outputs, ssr: true });
 
