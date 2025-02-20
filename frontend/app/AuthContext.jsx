8a51c6c0c61814e0b96f7d900faeb603647bb8a2
Yash Chokhani <yashchokhani95@gmail.com>
2025-02-21 00:50:26 +0530

    feat: get Access token function added

--- frontend/app/AuthContext.jsx
+++ frontend/app/AuthContext.jsx
@@ -1,8 +1,24 @@
 "use client";
 import React, { createContext, useState, useEffect } from "react";
-import { getCurrentUser } from "aws-amplify/auth";
+import { getCurrentUser, fetchAuthSession } from "aws-amplify/auth";
 const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [accessToken, setAccessToken] = useState("");
+  const [tokenExpiry, setTokenExpiry] = useState(0);
+  const getAccessToken = async () => {
+    if (tokenExpiry < Date.now() / 1000 && accessToken !== "")
+      return accessToken;
+    const session = await fetchAuthSession();
+    if (session) {
+      let token = session.tokens.accessToken.toString();
+      let expiry = session.tokens.accessToken.payload.exp;
+      setAccessToken(token);
+      setTokenExpiry(expiry);
+      return token;
+    }
+  };
   useEffect(() => {
     getCurrentUser()
       .then((user) => {
@@ -13,11 +29,15 @@ const AuthProvider = ({ children }) => {
       })
       .catch((err) => {});
   }, []);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [userId, setUserId] = useState("");
   return (
     <AuthContext.Provider
-      value={{ userId, setUserId, isAuthenticated, setIsAuthenticated }}
+      value={{
+        userId,
+        setUserId,
+        isAuthenticated,
+        setIsAuthenticated,
+        getAccessToken,
+      }}
     >
       {children}
     </AuthContext.Provider>
